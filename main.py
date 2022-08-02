49fa08329bb5ef41724760b272c6f85a896297fd
winnerwannabe <70723880+winnerwannabe@users.noreply.github.com>
2022-08-02 03:49:26 +0000

    made the first character random and added a number to signify how far past og char-characters in the message

--- main.py
+++ main.py
@@ -1,29 +1,50 @@
-import time, sys, os
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+import sys, os, random
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+specialchars = [" ", ".", "!", "?", ",", "-", ":"]
 store = []
-chars = 0
-forward_and_back = True
+randnum = random.randint(0, 35)
+chars = randnum-1
+forward_and_back = False
 
-def print_quickly(str):
-    for char in str:
-      time.sleep(0)
+def print_quickly(char):
       sys.stdout.write(char)
-      sys.stdout.flush()
 
-print('type a character. if finished type "done".')
-message = input(">")
-
-for c in range(len(message)):
-  for i in range(26):
-    if message[c] == alphabet[i]:
-      if forward_and_back == True:
-        store.append(alphabet[((i-chars) % 26)])
-        forward_and_back = False
+while True:
+  print('type a your message. if finished type "done".')
+  message = input(">")
+  chars += len(message)
+  while chars > 36:
+    chars -= 36
+  
+  for c in range(len(message)):
+    chars+=1
+    for i in range(36): #repeats 36 times b/c there are 36 things in alphabet
+      if message[c] == alphabet[i]: #checks if character in message is in alphabet. if it does it will ether add that letter plus or minus chars to store. otherwise it will send it to special chars to see if it is in there. if not it will not not add to store.
+        if forward_and_back == True:
+          store.append(alphabet[((i-chars) % 36)])#goes backwards from i to c. so if i was 2 and c was 1 it would go back to 1 and result in a b/c b back 1 is a 
+          forward_and_back = False
+          break
+        else:
+          store.append(alphabet[((i+chars) % 36)])#opposite of above
+          forward_and_back = True
+          break
+    for i in range(len(specialchars)): #checks if character is in special chars. if it is it adds it to store. otherwise it does not change store.
+      if message[c] == specialchars[i]:
+        store.append(specialchars[i])
         break
-      else:
-        store.append(alphabet[((i+chars) % 26)])
-        forward_and_back = True
-        break
-for i in range(store):
-  print_quickly(i)
-print()
+  store.insert(0, str(randnum))
+  for i in store:
+    print_quickly(i)
+  
+  print("\nwould you like to input another code? (y/n must use lower case)")
+  while True:
+    again = input(">")
+    if again == "y" or again == "n":#if again gets an acceptable awnser it breaks the while true and allows the program to asses the respons
+      break
+  if again == "y": #resets all the nessisary variables and reruns the code from the top while loop
+    os.system("clear")
+    store = []
+    chars = 0
+    forward_and_back = False
+  else: #exits the program
+    exit()
